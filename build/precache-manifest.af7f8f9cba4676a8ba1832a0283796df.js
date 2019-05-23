cc90d5fa0d0b7917f6847a08a53d655e77d29a6d
Dan Stevens <stevens.a.danny@gmail.com>
2019-05-23 09:42:40 -0500

    testing

---
+++ build/precache-manifest.af7f8f9cba4676a8ba1832a0283796df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bafcd0e00d394c665792dc49f5468128",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4f53d14743ce3c89c1e7",
+    "url": "/portfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b000fccb79e89af31dc4",
+    "url": "/portfolio/static/js/2.abf43ca4.chunk.js"
+  },
+  {
+    "revision": "4f53d14743ce3c89c1e7",
+    "url": "/portfolio/static/js/main.8afb41d1.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
