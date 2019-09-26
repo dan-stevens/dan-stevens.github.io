e2d35d289f34d9c2da1dda78058f511fce1145ad
Dan Stevens <stevens.a.danny@gmail.com>
2019-09-26 16:21:44 -0500

    Updates

---
+++ precache-manifest.bd19ad248c0efafc69930afeb3a2bedb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1609ff13651f41ff9a2200554a280ded",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d771b3c4c9c4fa0785c9",
+    "url": "/portfolio/static/css/main.bc8b5423.chunk.css"
+  },
+  {
+    "revision": "b989dac9dbb7bee4108c",
+    "url": "/portfolio/static/js/2.ebecf277.chunk.js"
+  },
+  {
+    "revision": "d771b3c4c9c4fa0785c9",
+    "url": "/portfolio/static/js/main.e98b042e.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "6b198fbee91e0ce28229a61d0a229ee4",
+    "url": "/portfolio/static/media/bio.6b198fbe.png"
+  },
+  {
+    "revision": "e068215580178dd07c23d8ebdd5cd92c",
+    "url": "/portfolio/static/media/gizmo.e0682155.svg"
+  },
+  {
+    "revision": "ea24c539d00f00d740196c357492de11",
+    "url": "/portfolio/static/media/wheel.ea24c539.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.172224339633e3b6783b385e5847e17a.js"
+  "/portfolio/precache-manifest.bd19ad248c0efafc69930afeb3a2bedb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
